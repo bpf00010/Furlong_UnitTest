67767c130f663d6698e07547a5ab876b1758f5a5
Brenden Furlong <bpf00010@mail.wvu.edu>
2026-04-23 14:29:00 -0400

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Furlong_UnitTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
